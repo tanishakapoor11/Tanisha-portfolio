4bb3b4584e84b5e8e84d7513ae42f6233083295a
Tanisha Kapoor <tanishakapoor2020@gmail.com>
2022-11-28 04:02:36 +0530

    Updated home page (1)

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,16 +1,19 @@
 import LogoTitle from '../../assets/images/logo-t.png';
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss';
 
 const Home = () => {
     return (
         <div className="container home-page">
             <div className="text-zone">
-                <h1>Hi, <br /> I'm 
+                <h1>Hi, <br /> I'm <tab />
                 <img src={LogoTitle} alt="developer" />
                 anisha
                 <br />
                 Web Developer
                 </h1>
                 <h2>Full Stack Developer | Data Scientist | UI/UX Designer</h2>
+                <Link to = "/contact" className='flat-button' >CONTACT ME</Link>
             </div>
         </div>
     );
